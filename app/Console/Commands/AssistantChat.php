8ab38bdc23aade0788ed552956c10472bb4d62ae
aralim11 <aralim11@gmail.com>
2026-06-10 19:05:37 +0600

    working on infinite input

--- app/Console/Commands/AssistantChat.php
+++ app/Console/Commands/AssistantChat.php
@@ -6,6 +6,8 @@ use Illuminate\Console\Attributes\Description;
 use Illuminate\Console\Attributes\Signature;
 use Illuminate\Console\Command;
 
+use function Laravel\Prompts\text;
+
 #[Signature('chat')]
 #[Description('Command description')]
 class AssistantChat extends Command
@@ -16,7 +18,16 @@ class AssistantChat extends Command
     public function handle()
     {
         while (true) {
-            echo 'Ask Me question.';
+            $input = $this->ask('Ask me a question (type exit to stop)');
+            $name = text('What is your name?');
+
+            if ($input === 'exit') {
+                $this->info('Goodbye 👋');
+                break;
+            }
+
+            // $this->error('Something went wrong.');
+            // $this->info("You asked: $input");
         }
     }
 }
